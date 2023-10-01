1139499484a1301d8aa0f8b96c085ebac516a2b0
NehemieMbg <nehemie.mbg@gmail.com>
2023-10-01 20:47:45 +0200

    base url

--- career-compass/apps/client/src/utils/customFetch.ts
+++ career-compass/apps/client/src/utils/customFetch.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const customFetch = axios.create({
-  baseURL: '/api/v1',
+  baseURL: 'https://career-compass-client.vercel.app/api/v1',
 });
 
 export default customFetch;
