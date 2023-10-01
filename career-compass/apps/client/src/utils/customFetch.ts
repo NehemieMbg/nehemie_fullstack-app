3b84fbc192d3f8bfe14625801c3c0cf81f566f88
NehemieMbg <nehemie.mbg@gmail.com>
2023-10-01 21:49:02 +0200

    modifying vite config

--- career-compass/apps/client/src/utils/customFetch.ts
+++ career-compass/apps/client/src/utils/customFetch.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const customFetch = axios.create({
-  baseURL: 'https://career-compass-client.vercel.app/api/v1',
+  baseURL: 'career-compass-server.vercel.app/api/v1',
 });
 
 export default customFetch;
--- career-compass/apps/client/vite.config.ts
+++ career-compass/apps/client/vite.config.ts
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://career-compass-server.vercel.app/api',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
 });
