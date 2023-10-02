6be9bb6d9a9a82d26f8ef3207c09eb76e476a76f
NehemieMbg <nehemie.mbg@gmail.com>
2023-10-02 08:42:18 +0200

    Using one cors

--- career-compass/apps/server/server.js
+++ career-compass/apps/server/server.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev')); // To log info about requests
 }
 
-app.use(cors());
+// app.use(cors());
 app.use(
   cors({
     origin: 'https://career-compass-client.vercel.app',
