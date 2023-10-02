0e49c87505be54caf857f3549c4af2fa95125d3b
NehemieMbg <nehemie.mbg@gmail.com>
2023-10-02 21:54:05 +0200

    helmet

--- career-compass/apps/server/routes/authRouter.js
+++ career-compass/apps/server/routes/authRouter.js
@@ -4,7 +4,7 @@ import {
   validateRegisterInput,
   validateLoginInput,
 } from '../middleware/validationMiddleware.js';
-import rateLimiter from 'express-rate-limit';
+import rateLimiter from 'express-rate-limit/dist/index.cjs';
 
 const apiLimiter = rateLimiter({
   windowMs: 15 * 60 * 1000,
